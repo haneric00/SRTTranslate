ed39ac8b18d21febed8b7eba23ad062400cb8fbc
LucasL53 <57372991+LucasL53@users.noreply.github.com>
2023-08-13 02:09:36 -0700

    Tested EN-JA-KO
    added docstrings

--- SRTtoDocx.py
+++ SRTtoDocx.py
@@ -1,11 +1,6 @@
-import pysrt
-from docx import Document
-from docx.shared import Pt
-import deepl
 import os
-
-# My API key is stored in an environment variable
-translator = deepl.Translator(os.getenv("DEEPL_AUTH_KEY"))
+import pysrt
+from deepl import Translator
 
 def srt_translate(srt_file, target_lang):
     '''
@@ -16,22 +11,16 @@ def srt_translate(srt_file, target_lang):
     target_lang: the language code for the target language (e.g. 'JA' for Japanese)
     '''
     subs = pysrt.open(srt_file)
+    translator = Translator(os.environ['DEEPL_AUTH_KEY'])
     for sub in subs:
-        sub.text = translator.translate_text(sub.text, target_lang=target_lang)
+        sub.text = translator.translate_text(sub.text, target_lang = target_lang)
     new_srt_file = srt_file[:-4] + '-' + target_lang + '.srt'
     subs.save(new_srt_file)
     print('done')
 
-def add_to_glossary(glossary_name, word_dict):
-    pass
-
-def translate_with_glossary(docx_file, glossary_name):
-    pass
-
-    
-
 def main():
-    srt_translate('./day_in_my_life_7.17.srt', 'KO')
+    # Example usage
+    srt_translate('file.srt', 'KO')
 
 if __name__ == '__main__':
     main()
